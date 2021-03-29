3a2b4db85fd6869ae8833a0ab69c3ae4c95315d6
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-03-29 15:04:29 +0200

    feat: added ChessRoyaleRepository class

--- src/main/java/fr/univ_smb/isc/m1/chess_royale/infrastructure/persistence/ChessRoyaleRepository.java
+++ src/main/java/fr/univ_smb/isc/m1/chess_royale/infrastructure/persistence/ChessRoyaleRepository.java
@@ -1,2 +1,11 @@
-package fr.univ_smb.isc.m1.chess_royale.infrastructure.persistence;public class ChessRoyaleRepository {
+package fr.univ_smb.isc.m1.chess_royale.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChessRoyaleRepository extends JpaRepository<ChessRoyaleUser, Long> {
+
+    ChessRoyaleUser findByName(String name);
+
 }
