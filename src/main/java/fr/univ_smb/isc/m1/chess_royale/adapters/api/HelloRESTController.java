166c2549616bb76f7971ffbde06a8375922e3ad6
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-29 00:11:08 +0200

    refact: moved /hello/logout to /logout

--- src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/api/HelloRESTController.java
+++ src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/api/HelloRESTController.java
@@ -1,14 +1,9 @@
 package fr.univ_smb.isc.m1.chess_royale.adapters.api;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
 @RestController
@@ -23,15 +18,6 @@ public class HelloRESTController
     @GetMapping("admin")
     public String helloAdmin(Principal principal) {
         return "Hello admin : " + principal.getName();
-    }
-
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
-        var auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/login?logout"; //You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 
 }
