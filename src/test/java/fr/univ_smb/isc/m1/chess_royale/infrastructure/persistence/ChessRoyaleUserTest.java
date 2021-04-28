2ce07cd1e847ded7b5c4852b38e3408393a7b783
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-28 16:34:10 +0200

    test

--- src/test/java/fr/univ_smb/isc/m1/chess_royale/infrastructure/persistence/ChessRoyaleUserTest.java
+++ src/test/java/fr/univ_smb/isc/m1/chess_royale/infrastructure/persistence/ChessRoyaleUserTest.java
@@ -1,5 +1,6 @@
 package fr.univ_smb.isc.m1.chess_royale.infrastructure.persistence;
 
+import fr.univ_smb.isc.m1.chess_royale.application.ChessRoyaleUserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ChessRoyaleUserTest {
 
+    private ChessRoyaleGame game = new ChessRoyaleGame("testGame");
+    private ChessRoyaleUser user = new ChessRoyaleUser("username", "test", "testToken");
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void subscribe() {
+        //var participant = new ChessRoyaleUserService();
+
+        //user.subscribe(game);
     }
 
     @Test
