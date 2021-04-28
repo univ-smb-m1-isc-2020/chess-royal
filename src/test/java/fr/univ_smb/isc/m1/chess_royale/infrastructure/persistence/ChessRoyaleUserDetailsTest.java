04b7b54c8ba57ac5eb10243012f99272edb8ff16
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-28 19:24:51 +0200

    feat: test

--- src/test/java/fr/univ_smb/isc/m1/chess_royale/infrastructure/persistence/ChessRoyaleUserDetailsTest.java
+++ src/test/java/fr/univ_smb/isc/m1/chess_royale/infrastructure/persistence/ChessRoyaleUserDetailsTest.java
@@ -1,7 +1,19 @@
 package fr.univ_smb.isc.m1.chess_royale.infrastructure.persistence;
 
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +27,15 @@ class ChessRoyaleUserDetailsTest {
     public void setup() throws Exception {
         this.userTest =  new ChessRoyaleUser("NomTest", "none","token");
         this.chessRoyaleUserDetailsTest = new ChessRoyaleUserDetails(userTest);
+    }
+
+    @Test
+    void should_return_permissions() {
+        var auth = chessRoyaleUserDetailsTest.getAuthorities();
+        var expected = new ArrayList<SimpleGrantedAuthority>();
+        expected.add(new SimpleGrantedAuthority("USER"));
+
+        assertEquals(expected, auth);
     }
 
     @Test
