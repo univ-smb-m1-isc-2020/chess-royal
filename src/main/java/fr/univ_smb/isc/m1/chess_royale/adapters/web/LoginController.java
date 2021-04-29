5a7d2079ccb4f13f5f3ecb2ad5d8d01e6d061340
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-29 13:11:34 +0200

    chore: fix code smells

--- src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/web/LoginController.java
+++ src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/web/LoginController.java
@@ -4,8 +4,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
