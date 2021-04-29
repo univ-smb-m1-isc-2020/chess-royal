9a76255da120914870c12a698d8387c5c4120c6f
graveaccentquote <34614915+graveaccentquote@users.noreply.github.com>
2021-04-29 14:27:59 +0200

    feat: added post request to launch a d-d-d-duel

--- src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/web/HomeController.java
+++ src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/web/HomeController.java
@@ -1,9 +1,13 @@
 package fr.univ_smb.isc.m1.chess_royale.adapters.web;
 
 import fr.univ_smb.isc.m1.chess_royale.application.ChessRoyaleClientService;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.security.Principal;
 
@@ -21,5 +25,30 @@ public class HomeController {
         model.addAttribute("users", chessRoyaleClientService.users());
         model.addAttribute("connectedUserName", principal.getName());
         return "home";
+    }
+
+    @GetMapping(value="/post-test")
+    public String postRequestTest()
+    {
+        var url = "https://lichess.org/api/challenge/Owydoo";
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        headers.add("Authorization", "Bearer PlbPWXEbwRg4Qb5J");
+
+        var entity = new HttpEntity<String>("Headers", headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try
+        {
+            restTemplate.postForEntity(url, entity , String.class );
+            return "redirect:/home";
+        }
+        catch (Exception e)
+        {
+            return "redirect:/too-many-request";
+        }
     }
 }
--- src/main/java/fr/univ_smb/isc/m1/chess_royale/adapters/web/MainController.java
+++
@@ -1,7 +0,0 @@
-package fr.univ_smb.isc.m1.chess_royale.adapters.web;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MainController {
-}
